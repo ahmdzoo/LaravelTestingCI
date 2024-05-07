e3a4499c090f8c889d0d1862be87fbdc1c6c2ea4
ahmdzoo <ahmdzoo>
2024-05-07 10:05:34 +0700

    test ci benar

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -17,6 +17,6 @@ class ExampleTest extends TestCase
     $a = 10;
     $b = 20;
     $jumlahkan = $a + $b;
-    $this->assertTrue($jumlahkan == 20);
+    $this->assertTrue($jumlahkan == 30);
     }
 }
